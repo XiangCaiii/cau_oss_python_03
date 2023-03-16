902dc36531ec59e63a4752f23134eab259666e1a
XiangCaiii <127946732+XiangCaiii@users.noreply.github.com>
2023-03-16 19:54:58 +0800

    Create volume_calc.py
    중앙대학교 오픈소스SW와 파이썬프로그래밍 03 분반 1주차, 박스의 부피 계산

---
+++ volume_calc.py
@@ -0,0 +1,14 @@
+print('가로를 입력하십시오!')
+length = input("가로:")
+print('세로를 입력하십시오!')
+width = input("세로")
+print('높이를 입력하십시오!')
+height = input("높이")
+
+length = float(length)
+width  = float(width)
+height = float(height)
+
+V = length * width * height
+
+print("부피는", V , "입니다.")
